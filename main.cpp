b663158757ef5c02a52521f3d01c03aff341fe8d
Alice (Ibrahima L) <leilakot69@gmail.com>
2023-06-30 15:03:42 +0300

    code: syncA

--- main.cpp
+++ main.cpp
@@ -9,18 +9,7 @@ int main()
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
-<<<<<<< HEAD
-         << "A * B = " << a * b << '\n';
-=======
          << "A * B = " << a * b << '\n'
          << "A / B + A = " << a / b + a << '\n';
->>>>>>> 458b03e24a4b78715b9c16be2ce719ef0d4ba6a5
-    if ( a > b ){
-        cout << a << '\n';
-    }
-    else
-    {
-        cout << b << '\n';
-    }
     return 0;
 }
